4b85a0f64f40c683e49169a210932c6893c750b4
jhessie27 <jessicandoh38@gmail.com>
2022-09-12 21:20:21 -0700

    Task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  * Description: A program that prints all possible combinations
@@ -9,31 +9,35 @@ int main(void)
 {
 		int a, b;
 
-			for (a = 0; a <= 9; a++)
-			{
+				for (a = 0; a <= 9; a++)
+				{
 				for (b = 0; b <= 9; b++)
 				{
 					if (a < b)
 					{
-						putchar(a + '0');	
+						
+						putchar(a + '0');
 						putchar(b + '0');
 
-					if (a != 8 || (a == 8 && b != 9))
-					{
-		
-							putchar(',');	
+						if (a != 8 || (a == 8 && b != 9))
+						{
+
+							
+							putchar(',');
 							putchar(' ');
 
 						}
+
 				}
+
 		}
+
 	}
 
 	putchar('\n');
 	return (0);
 
 }
-
 
 
 

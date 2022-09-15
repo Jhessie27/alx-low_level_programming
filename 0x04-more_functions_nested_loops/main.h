bd6608338aae66b7f2381673ff77dd23158d3bb7
jhessie27 <jessicandoh38@gmail.com>
2022-09-15 09:26:51 -0700

    i_supper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *main - header file for prototypes
+ *void prototypes (void)
+ *int prototypes (int)
+ */
+
+0-isupper.c
+1-isdigit.c
+2-mul.c
+3-print_numbers.c
+4-print_most_numbers.c
+5-more_numbers.c
+6-print_line.c
+7-print_diagonal.c
+8-print_square.c
+9-fizz_buzz.c
+10-print_triangle.c
+101-print_number.c
+100-prime_factor.c
+
+endif

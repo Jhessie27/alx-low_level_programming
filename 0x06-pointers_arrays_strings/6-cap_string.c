1efeb06e4cd1e1e3a9f0ce5ed426b90885a933ca
jhessie27 <jessicandoh38@gmail.com>
2022-09-26 20:08:43 -0700

    101-crackme_password

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,14 +6,13 @@
  *
  * Return: pointer to string
  */
-
 char *cap_string(char *str)
-{
+{;
 	int index = 0;
 
 	while (str[index])
 	{
-		while ((str[index] >= 'a' && str[index] <= 'z'))
+		while (:(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
 		if (str[index - 1] == ' ' ||
